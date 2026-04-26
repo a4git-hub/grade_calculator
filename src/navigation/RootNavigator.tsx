46ad55b125da0538cfc3fde98bf8a4f24aeed49e
Bhupender Singh <bhupender@xcel.dev>
2026-04-26 03:53:19 -0700

    nav: register IcSpike modal screen on root stack

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../types';
 import { OnboardingNavigator } from './OnboardingNavigator';
 import { MainNavigator } from './MainNavigator';
+import { IcSpikeScreen } from '../screens/main/IcSpikeScreen';
 import { useTheme } from '../context/ThemeContext';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -21,6 +22,11 @@ export function RootNavigator() {
       >
         <Stack.Screen name="Onboarding" component={OnboardingNavigator} />
         <Stack.Screen name="Main"       component={MainNavigator} />
+        <Stack.Screen
+          name="IcSpike"
+          component={IcSpikeScreen}
+          options={{ presentation: 'modal', animation: 'slide_from_bottom' }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
